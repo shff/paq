5bc6fdb49899ba025668c7c12e25296036d18f95
Silvio Henrique Ferreira <shferreira@me.com>
2019-11-24 18:17:54 +0000

    Change return type of resolve()

--- resolve/src/lib.rs
+++ resolve/src/lib.rs
@@ -1,7 +1,7 @@
 extern crate json;
 mod utils;
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::{read_to_string};
 
 use utils::PathExt;
@@ -13,7 +13,7 @@ pub enum Error {
     Internal,
 }
 
-pub fn resolve(name: String, context: &Path) -> Result<String, Error> {
+pub fn resolve(name: String, context: &Path) -> Result<PathBuf, Error> {
     if name.is_empty() {
         return Err(Error::NameEmpty);
     }
@@ -42,16 +42,16 @@ pub fn resolve(name: String, context: &Path) -> Result<String, Error> {
     }
 }
 
-fn load(path: &Path) -> Option<String> {
+fn load(path: &Path) -> Option<PathBuf> {
     if path.is_file() {
-        return Some(path.to_string_lossy().to_string());
+        return Some(path.to_path_buf());
     }
 
     let extensions = vec!["js", "mjs", "json"];
     for extension in extensions {
         let new_path = path.with_extension(extension);
         if new_path.is_file() {
-            return Some(new_path.to_string_lossy().to_string());
+            return Some(new_path);
         }
     }
 
@@ -77,74 +77,74 @@ fn test_resolve() {
 
     assert_eq!(
         resolve("./counter".to_string(), &fixtures.join("relative/index.js")),
-        Ok(fixtures.join("relative/counter").to_string_lossy().to_string())
+        Ok(fixtures.join("relative/counter"))
     );
     assert_eq!(
         resolve("./counter".to_string(), &fixtures.join("relative-js/index.js")),
-        Ok(fixtures.join("relative-js/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("relative-js/counter.js"))
     );
     assert_eq!(
         resolve("./counter".to_string(), &fixtures.join("relative-mjs/index.mjs")),
-        Ok(fixtures.join("relative-mjs/counter.mjs").to_string_lossy().to_string())
+        Ok(fixtures.join("relative-mjs/counter.mjs"))
     );
     assert_eq!(
         resolve("../counter".to_string(), &fixtures.join("parent-js/entry/index.js")),
-        Ok(fixtures.join("parent-js/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("parent-js/counter.js"))
     );
     assert_eq!(
         resolve("./counter/counter".to_string(), &fixtures.join("relative-nested/index.js")),
-        Ok(fixtures.join("relative-nested/counter/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("relative-nested/counter/counter.js"))
     );
     assert_eq!(
         resolve("../counter".to_string(), &fixtures.join("relative-dir/entry/index.js")),
-        Ok(fixtures.join("relative-dir/counter/index.js").to_string_lossy().to_string())
+        Ok(fixtures.join("relative-dir/counter/index.js"))
     );
     assert_eq!(
         resolve("../counter/counter".to_string(), &fixtures.join("parent-nested/entry/index.js")),
-        Ok(fixtures.join("parent-nested/counter/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("parent-nested/counter/counter.js"))
     );
     assert_eq!(
         resolve("./".to_string(), &fixtures.join("pkginfo-basic/index.js")),
-        Ok(fixtures.join("pkginfo-basic/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-basic/counter.js"))
     );
     assert_eq!(
         resolve(".".to_string(), &fixtures.join("pkginfo-basic/index.js")),
-        Ok(fixtures.join("pkginfo-basic/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-basic/counter.js"))
     );
     assert_eq!(
         resolve("./counter".to_string(), &fixtures.join("pkginfo-nested/index.js")),
-        Ok(fixtures.join("pkginfo-nested/counter/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-nested/counter/counter.js"))
     );
     assert_eq!(
         resolve("../".to_string(), &fixtures.join("pkginfo-parent/entry/index.js")),
-        Ok(fixtures.join("pkginfo-parent/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-parent/counter.js"))
     );
     assert_eq!(
         resolve("..".to_string(), &fixtures.join("pkginfo-parent/entry/index.js")),
-        Ok(fixtures.join("pkginfo-parent/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-parent/counter.js"))
     );
     assert_eq!(
         resolve(".".to_string(), &fixtures.join("pkginfo-dot/index.js")),
-        Ok(fixtures.join("pkginfo-dot/index.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-dot/index.js"))
     );
     assert_eq!(
         resolve("..".to_string(), &fixtures.join("pkginfo-dot/entry/index.js")),
-        Ok(fixtures.join("pkginfo-dot/index.js").to_string_lossy().to_string())
+        Ok(fixtures.join("pkginfo-dot/index.js"))
     );
     assert_eq!(
         resolve("package".to_string(), &fixtures.join("modules-basic/index.js")),
-        Ok(fixtures.join("modules-basic/node_modules/package/index.js").to_string_lossy().to_string())
+        Ok(fixtures.join("modules-basic/node_modules/package/index.js"))
     );
     assert_eq!(
         resolve("package".to_string(), &fixtures.join("modules-file/index.js")),
-        Ok(fixtures.join("modules-file/node_modules/package.js").to_string_lossy().to_string())
+        Ok(fixtures.join("modules-file/node_modules/package.js"))
     );
     assert_eq!(
         resolve("package".to_string(), &fixtures.join("modules-pkginfo/index.js")),
-        Ok(fixtures.join("modules-pkginfo/node_modules/package/entry.js").to_string_lossy().to_string())
+        Ok(fixtures.join("modules-pkginfo/node_modules/package/entry.js"))
     );
     assert_eq!(
         resolve("package/lib/counter".to_string(), &fixtures.join("modules-nested/index.js")),
-        Ok(fixtures.join("modules-nested/node_modules/package/lib/counter.js").to_string_lossy().to_string())
+        Ok(fixtures.join("modules-nested/node_modules/package/lib/counter.js"))
     );
 }
