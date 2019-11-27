26070d0039660a4b54dd34692710043bf7b07ef1
Silvio Henrique Ferreira <shferreira@me.com>
2019-11-27 21:16:44 +0000

    Add queue

---
+++ bundler/src/queue.rs
@@ -0,0 +1,45 @@
+use std::hash::{Hash};
+use std::collections::{HashMap, VecDeque};
+
+pub fn run<A, B, F: FnMut(A) -> Result<(B, Vec<A>), String>>(first_job: A, mut perform: F) -> Result<HashMap<A, B>, String>
+where A: Clone + Eq + Hash, B: Clone {
+    let mut results = HashMap::<A, B>::default();
+    let mut queue = VecDeque::<A>::new();
+    queue.push_back(first_job);
+
+    while let Some(job) = queue.pop_front() {
+        let (result, new_jobs) = perform(job.clone())?;
+
+        results.insert(job, result);
+        for job in new_jobs {
+            if !results.contains_key(&job) {
+                queue.push_back(job);
+            }
+        }
+    };
+    Ok(results)
+}
+
+#[test]
+fn test_queue() {
+    let result = run(1, |num| {
+        match num {
+            1 => Ok(("one", vec![ 2 ])),
+            2 => Ok(("two", vec![])),
+            _ => Err("Failure".to_string())
+        }
+    });
+    assert_eq!(result.clone().unwrap().get(&1), Some(&"one"));
+    assert_eq!(result.clone().unwrap().get(&2), Some(&"two"));
+
+    let result = run("one", |num| { Ok((num, vec![])) });
+    assert_eq!(result.clone().unwrap().get(&"one"), Some(&"one"));
+
+    let result = run(1, |num| {
+        match num {
+            1 => Ok(("one", vec![ 2 ])),
+            _ => Err("Failure".to_string())
+        }
+    });
+    assert_eq!(result, Err("Failure".to_string()));
+}
