95fee4dc50faa13f771807f3c9d4f9d3687dc2bc
Silvio Henrique Ferreira <shferreira@me.com>
2019-12-04 23:11:15 +0000

    Add end-to-end testing with node.js

--- bundler/src/lib.rs
+++ bundler/src/lib.rs
@@ -29,8 +29,15 @@ fn test_bundler() {
         let result = bundle("index.js".to_string(), &fixtures.join(path)).expect("Error");
         assert!(result.contains(substring))
     }
-    assert_bundle("basic", "hello()");
+    fn assert_node(path: &str, value: &str) {
+        let fixtures = std::env::current_dir().unwrap().join("fixtures");
+        let result = bundle("index.js".to_string(), &fixtures.join(path)).expect("Error");
+        let output = std::process::Command::new("node").arg("-e").arg(result).output().expect("Error running node");
+        assert_eq!(String::from_utf8_lossy(&output.stdout), value);
+    }
+    assert_bundle("basic", "console.log('hello')");
     assert_bundle("with-dep", "/* math.js */");
     assert_bundle("double-quotes", "/* math.js */");
     assert_bundle("crazy-indent", "/* math.js */");
+    assert_node("basic", "hello\n");
 }
