eaae28730b6b95b9efdcee588ce00b78258a1db4
Silvio Henrique Ferreira <shferreira@me.com>
2019-12-05 22:48:12 +0000

    Remove horrible unique_id generator

--- bundler/src/writer.rs
+++ bundler/src/writer.rs
@@ -11,25 +11,20 @@ pub struct Module {
 }
 
 pub fn write(modules: &HashMap<PathBuf, Module>, entry_point: &Path) -> Result<String, Error> {
-    let mut map = HashMap::<PathBuf, usize>::new();
-    let entry_id = get_unique_id(&mut map, entry_point.to_path_buf());
+    let files: Vec<PathBuf> = modules.keys().cloned().collect();
 
     let mut w = String::new();
     write!(w, "{}", PRELUDE_JS).unwrap();
     for (file, module) in modules {
-        let filename = get_unique_id(&mut map, file.to_path_buf());
+        let filename = files.iter().position(|v| v == file).unwrap();
         let deps = json::stringify(module.deps.iter().map(|(dep, path)|
-            (dep.to_string(), get_unique_id(&mut map, path.to_path_buf()))
+            (dep.to_string(), files.iter().position(|v| v == path).unwrap())
         ).collect::<HashMap::<String, usize>>());
 
         write!(w, "__deps[{}] = {{ deps: {}, func: function(module, exports, require) {{\n{} \n}} }};", filename, deps, module.source).unwrap();
     }
+    let entry_id = files.iter().position(|v| v == entry_point).unwrap();
     write!(w, "__req(null)({}) }})()", entry_id).unwrap();
 
     Ok(w)
-}
-
-fn get_unique_id(map: &mut HashMap<PathBuf, usize>, item: PathBuf) -> usize {
-    let len = map.len();
-    *map.entry(item).or_insert(len)
 }
