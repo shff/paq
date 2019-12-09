3c5b146caa8581f5bdf1f437f07954b6f59f5c8a
Silvio Henrique Ferreira <shferreira@me.com>
2019-12-09 10:07:23 +0000

    Use ? instead of unwrap in some places

--- bundler/src/writer.rs
+++ bundler/src/writer.rs
@@ -14,17 +14,17 @@ pub fn write(modules: &HashMap<PathBuf, Module>, entry_point: &Path) -> Result<S
     let files: Vec<PathBuf> = modules.keys().cloned().collect();
 
     let mut w = String::new();
-    write!(w, "{}", PRELUDE_JS).unwrap();
+    write!(w, "{}", PRELUDE_JS)?;
     for (file, module) in modules {
         let filename = files.iter().position(|v| v == file).unwrap();
         let deps = json::stringify(module.deps.iter().map(|(dep, path)|
             (dep.to_string(), files.iter().position(|v| v == path).unwrap())
         ).collect::<HashMap::<String, usize>>());
 
-        write!(w, "__deps[{}] = {{ deps: {}, func: function(module, exports, require) {{\n{} \n}} }};", filename, deps, module.source).unwrap();
+        write!(w, "__deps[{}] = {{ deps: {}, func: function(module, exports, require) {{\n{} \n}} }};", filename, deps, module.source)?;
     }
     let entry_id = files.iter().position(|v| v == entry_point).unwrap();
-    write!(w, "__req(null)({}) }})()", entry_id).unwrap();
+    write!(w, "__req(null)({}) }})()", entry_id)?;
 
     Ok(w)
 }
