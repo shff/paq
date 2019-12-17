0b0b3b01c68a87d2784a248c9f7a20b71cdc7e24
Silvio Henrique Ferreira <shferreira@me.com>
2019-12-17 06:10:34 +0000

    Add end-to-end test cases

--- src/lib.rs
+++ src/lib.rs
@@ -61,8 +61,12 @@ fn test_bundler() {
     assert_bundle("with-dep", "/* math.js */");
     assert_bundle("double-quotes", "/* math.js */");
     assert_bundle("crazy-indent", "/* math.js */");
+    assert_bundle("with-modules", "PETER RULES");
+    assert_bundle("with-modules-2", "PartitionIter");
     assert_node("basic", "hello");
     assert_node("with-dep", "2");
     assert_node("double-quotes", "");
     assert_node("crazy-indent", "");
+    assert_node("with-modules", "Once upon a day there was a person, named Peter DeMartini\nHe is awesome!\nHave a nice day...\n \n \n \nPETER RULES!!!");
+    assert_node("with-modules-2", "0 1 2 3 4");
 }
