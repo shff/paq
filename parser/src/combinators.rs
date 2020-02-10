931840265db2c25687121511b03956205bca5698
Silvio Henrique Ferreira <shferreira@me.com>
2020-02-10 01:15:25 +0000

    Add tag combinator

---
+++ parser/src/combinators.rs
@@ -0,0 +1,30 @@
+pub type ParseResult<'a, T> = Result<(&'a str, T), (&'a str, ParserError)>;
+
+/// Recognizes a fixed string pattern.
+///
+/// If the input data matches the first argument, it will return a successful
+/// value containing the argument itself.
+///
+/// Otherwise it returns `Err((_, ParserError::Tag))`
+///
+/// # Example
+/// ```rust
+/// use js_parser::combinators::*;
+///
+/// let parser = tag("function");
+///
+/// assert_eq!(parser("function hello"), Ok((" hello", "function")));
+/// assert_eq!(parser("Something else"), Err(("Something else", ParserError::Tag)));
+/// assert_eq!(parser(""), Err(("", ParserError::Tag)));
+/// ```
+pub fn tag(tag: &'static str) -> impl Fn(&str) -> ParseResult<&str> {
+    move |i| match i.starts_with(tag) {
+        true => Ok((&i[tag.len()..], tag)),
+        false => Err((i, ParserError::Tag)),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ParserError {
+    Tag,
+}
