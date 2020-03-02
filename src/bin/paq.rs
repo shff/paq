f86416bad6a47d5c6873a0bc1b97ad86a98bd03b
Silvio Henrique Ferreira <shferreira@me.com>
2020-03-02 21:12:41 +0000

    Fix indentation

--- src/bin/paq.rs
+++ src/bin/paq.rs
@@ -3,8 +3,8 @@ use std::path::PathBuf;
 extern crate paq;
 
 fn main() {
-	let entry = std::env::args().nth(1).expect("Missing argument: entry point");
-  let entry = PathBuf::from(entry).canonicalize().expect("Can't find entry point");
-	let result = paq::bundle(&entry).expect("Error during bundling");
-	println!("{}", result);
+    let entry = std::env::args().nth(1).expect("Missing argument: entry point");
+    let entry = PathBuf::from(entry).canonicalize().expect("Can't find entry point");
+    let result = paq::bundle(&entry).expect("Error during bundling");
+    println!("{}", result);
 }
