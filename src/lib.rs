5a3fcae085d2c41062b5eeaa422591b457e1df07
Silvio Henrique Ferreira <shferreira@me.com>
2020-03-02 21:50:05 +0000

    Use js module for pseudo-parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,24 @@
+extern crate regex;
+
+pub mod js;
 pub mod queue;
 pub mod resolve;
-pub mod js;
 
 use std::collections::HashMap;
-use std::fs::read_to_string;
-use std::path::{Path, PathBuf};
-
-#[derive(Debug, Clone)]
-struct Module {
-    source: String,
-    deps: HashMap<String, PathBuf>,
-}
+use std::path::PathBuf;
 
 pub fn bundle(entry: &PathBuf) -> Result<String, queue::Error> {
     let modules = queue::run(entry.clone(), |path| {
-        let re = regex::Regex::new(r#"require\s*\(\s*['"](.+?)['"]\s*\)"#).unwrap();
-        let source = read_to_string(&path).expect("Can't open file");
-        let deps = re.captures_iter(&source).map(|dep| {
-            (dep[1].to_string(), resolve::resolve(dep[1].to_string(), &path.parent().unwrap()).unwrap())
-        }).collect::<HashMap::<String, PathBuf>>();
-        let modules = deps.values().cloned().collect();
+        let module = js::parse(&path).unwrap();
+        let modules = module.deps.values().cloned().collect();
 
-        Ok((Module { source, deps }, modules))
+        Ok((module, modules))
     })?;
     Ok(write(&modules, &entry))
 }
 
-fn write(modules: &HashMap<PathBuf, Module>, entry: &Path) -> String {
-    let get_id = |file: &Path| modules.keys().position(|v| v == file).unwrap();
+fn write(modules: &HashMap<PathBuf, js::Module>, entry: &PathBuf) -> String {
+    let get_id = |file: &PathBuf| modules.keys().position(|v| v == file).unwrap();
     let prelude = "
 (function() {
   var __req = self => dep => {
