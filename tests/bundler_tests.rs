aa094148eda6a9d240f6b6abbba0b95720bbb34b
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2020-04-10 19:55:22 +0200

    Add Writer (#15)

--- tests/bundler_tests.rs
+++ tests/bundler_tests.rs
@@ -28,9 +28,9 @@ fn test_bundler() {
     assert_bundle("with-modules", "PETER RULES");
     assert_bundle("with-modules-2", "PartitionIter");
     assert_node("basic", "hello");
-    // assert_node("with-dep", "2");
-    // assert_node("double-quotes", "");
-    // assert_node("crazy-indent", "");
-    // assert_node("with-modules", "Once upon a day there was a person, named Peter DeMartini\nHe is awesome!\nHave a nice day...\n \n \n \nPETER RULES!!!");
-    // assert_node("with-modules-2", "0 1 2 3 4");
+    assert_node("with-dep", "2");
+    assert_node("double-quotes", "");
+    assert_node("crazy-indent", "");
+    assert_node("with-modules", "Once upon a day there was a person, named Peter DeMartini\nHe is awesome!\nHave a nice day...\n \n \n \nPETER RULES!!!");
+    assert_node("with-modules-2", "0 1 2 3 4");
 }
