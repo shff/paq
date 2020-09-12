876223cf8a411dea6b6d399104defd9cdc36407e
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2020-09-13 01:48:11 +0200

    Get 100% coverage (#32)

--- src/bin/bundler.rs
+++ src/bin/bundler.rs
@@ -1,22 +1,17 @@
 use std::path::PathBuf;
 
 fn main() {
-    let mut entry = PathBuf::from("index.js");
-
-    let mut args = std::env::args().skip(1);
-    while let Some(arg) = args.next() {
-        match &*arg {
-            "-e" | "--entry" => entry = args.next().map(PathBuf::from).unwrap(),
-            "-v" | "--version" => {
-                println!("{} v{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
-                return;
-            }
-            e => entry = PathBuf::from(e),
-        }
-    }
+    let default = "index.js".to_string();
+    let arg = std::env::args().nth(1).unwrap_or(default);
+    let entry = PathBuf::from(arg);
 
     match paq::bundle(&entry) {
         Ok(data) => println!("{}", data),
         Err(err) => eprintln!("{}", err),
     };
+}
+
+#[test]
+fn test_bundler_main() {
+    assert_eq!(main(), ());
 }
